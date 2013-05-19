a06979ec2d7b8f644855cee34fc4dafbbf0bdd23
James Lyden <james@lyden.org>
2013-05-19 02:13:34 -0700

    Change external scripts to only use read-only URLs

--- make-externs.sh
+++ make-externs.sh
@@ -3,6 +3,6 @@
 printf "#!/bin/bash\n\n"
 for dir in $(find vim/bundle/ -name ".git"); do
 	printf "git clone %s %s\n" \
-		"$(grep '.git$' $dir/config | cut -d= -f2)" \
+		"$(grep '.git$' $dir/config | cut -d= -f2 | sed -e 's?git@github.com:?git://github.com/?')" \
 		"$(echo $dir | sed -e 's:/.git::')"
 done
